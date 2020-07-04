0689f57252389de342c8bb5f50d1cb93d2ff5f4e
georgiev-ventsi <v.g.al.hq@gmail.com>
2020-07-04 11:01:54 +0300

    import Child and pass component's state

--- CodeCademy/react_build-a-stateful-component-class/Parent.js
+++ CodeCademy/react_build-a-stateful-component-class/Parent.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { Child } from './Child'
 
 class Parent extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = { name: 'Frarthur' }
-    }
-  
-    render() {
-          return <div></div>
-      }
+  constructor(props) {
+    super(props)
+    this.state = { name: 'Frarthur' }
   }
+
+  render() {
+        return <Child name={this.state.name}/>
+    }
+}
